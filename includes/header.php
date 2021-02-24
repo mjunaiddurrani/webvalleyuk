d46daf7a878739d65e19fcc8bdfa889478bb5763
mjunaiddurrani <akashrajput9@hotmail.com>
2021-02-24 21:57:48 +0500

    upasdte

---
+++ includes/header.php
@@ -0,0 +1,76 @@
+<header class="header-main">
+  <div class="top-bar d-none d-lg-block">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="secondlogo">
+            <a href="index.html">
+              <img class="img-fluid fblack" src="assets/images/logo.svg" alt="*" />
+              <img class="img-fluid fwhite" src="assets/images/logof.svg" alt="*" />
+            </a>
+          </div>
+          <div class="emailsec">
+            <a href="mailto:support@webprohub.co.uk"><span class="icon-envelope2"></span>support@webprohub.co.uk</a>
+          </div>
+         
+          
+          
+        </div>
+        <div class="col-lg-6">
+          <ul class="top-info-list">
+            <li><a href="tel:+441442902150"><img src="assets/images/uk-flag.png" alt="UK Flag" /> +44 144 290 2150 </a></li>
+            <li><a href="javascript:;" onclick="setButtonURL();" target="_self"><span class="icon-chat"></span> Live Chat</a> </li>
+            <li><a href="order.html" class="btn-main">Let's Get Started</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="nav-area-full">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2 mobile-res-logo">
+          <div class="logo">
+            <a href="index.html">
+              <img class="img-fluid" src="assets/images/logo.svg" alt="*">
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-12 d-flex ">
+          <div class="main-menu align-self-center d-none d-lg-block">
+            <ul class="forpgactive">
+              <li class="active"><a href="index.html">Home</a></li>
+              <li class=""><a href="ecommerce-website-design.html">Ecommerce Solutions</a></li>
+               <li class=""><a href="web-app-development.html">Web App Development</a></li>
+
+                   
+                    <li class=""><a href="video-animation.html">Video Animation</a></li>
+                    <li class=""><a href="combo-packages.html">Combo Packages</a></li>
+              <li class=""><a href="packages.html">Packages</a></li>
+              
+              <li class=""><a href="portfolio.html">Portfolio</a></li>
+              <li class=""><a href="contact.html">Contact</a></li>
+              <li class="has-submenu"><a href="javascript:;" class="menuside">More <i class="icon-plus"></i></a>
+              
+                <div class="dropdown">
+                  <ul>
+                    
+                    <li class=""><a href="website-design-prototype.html">Design & Prototype</a></li>
+              <!-- <li class=""><a href="https://webprohub.co.uk/web-app-development">Web App Development</a></li> -->
+              
+              <!--<li class=""><a href="https://webprohub.co.uk/website-mantainance">Website Maintenance</a></li>-->
+
+              
+              <li class=""><a href="branding.html">Branding</a></li>                     
+                    
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- <a href="https://webprohub.co.uk/" class="logo-main"> <img class="mobile-res-logo" src="assets/images/logo.svg" alt="*"></a> -->
+</header>
