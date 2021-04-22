2a62e87256c2d89cace31e4ee9770a8e00be1e5b
Murtuza Mehdi <murtuzamehdi334@gmail.com>
2021-04-22 17:01:13 +0500

    make changes

--- views/thank-you.php
+++ views/thank-you.php
@@ -13,6 +13,7 @@
         <div class="row text-center text-sm-left my-auto">
           <div class="col-lg-9">
             <h1 class="text-white mb-4 font-medium" style="position: relative;top: 200px;"><?php if(isset($_REQUEST['thanksMsg'])){ echo $_REQUEST['thanksMsg'];} ?></h1>
+            <h1 class="text-white mb-4 font-medium" style="position: relative;top: 200px;"><?php if(isset($_REQUEST['successMsg'])){ echo $_REQUEST['successMsg'];} ?></h1>
           </div>
         </div>
       </div>
