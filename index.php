a50e4016dca8ce0fe3da3c7926a50127a2b3d692
akashrajput9 <akashrajput9@hotmail.com>
2021-04-26 13:38:17 +0500

    duapte

--- index.php
+++ index.php
@@ -1,97 +1,3 @@
-<?php
-// ob_start();
-// session_start();
-
-
-// $url = $_SERVER['REQUEST_URI'];
-
-
-
-// $pieces = explode(' ', $url);
-// $last_word = array_pop($pieces);
-
-
-
-
-// $current_url = explode('?', $url);
-// $url = $current_url[0];
-
-
-// switch($url){
-//     case '/' :
-//         require __DIR__.'/views/home.php';
-//         break;
-//     case addslashes('/leads/') :
-//         require __DIR__.'/views/leads.php';
-//         break;
-//     case addslashes('/order/') :
-//         require __DIR__.'/views/order.php';
-//         break;
-//     case addslashes('/thank-you/') :
-//         require __DIR__.'/views/thankyou.php';
-//         break;
-//     case addslashes('/contact-us/') :
-//         require __DIR__.'/views/contact.php';
-//         break;
-//     case addslashes('/branding/') :
-//         require __DIR__.'/views/branding.php';
-//         break;
-//     case addslashes('/combo-packages/') :
-//         require __DIR__.'/views/combo-packages.php';
-//         break;
-//     case addslashes('/contact/') :
-//         require __DIR__.'/views/contact.php';
-//         break;
-//     case addslashes('/custom-quote/') :
-//         require __DIR__.'/views/custom-quote.php';
-//         break;
-//     case addslashes('/ecommerce-web-development/') :
-//         require __DIR__.'/views/ecommerce-website-design.php';
-//         break;
-//     case addslashes('/mobile-app-development/') :
-//         require __DIR__.'/views/mobile-application.php';
-//         break;
-//     case addslashes('/not-found/') :
-//         require __DIR__.'/views/not-found.php';
-//         break;
-//     case addslashes('/order/') :
-//         require __DIR__.'/views/order.php';
-//         break;
-//     case addslashes('/packages/') :
-//         require __DIR__.'/views/packages.php';
-//         break;
-//     case addslashes('/portfolio/') :
-//         require __DIR__.'/views/portfolio.php';
-//         break;
-//     case addslashes('/privacy-policy/') :
-//         require __DIR__.'/views/privacy-policy.php';
-//         break;
-//     case addslashes('/terms-conditions/') :
-//         require __DIR__.'/views/terms-conditions.php';
-//         break;
-//     case addslashes('/video-animation/') :
-//         require __DIR__.'/views/video-animation.php';
-//         break;
-//     case addslashes('/web-app-development/') :
-//         require __DIR__.'/views/web-app-development.php';
-//         break;
-//     case addslashes('/web-design-development/') :
-//         require __DIR__.'/views/web-design-development.php';
-//         break;
-//     // case addslashes('/website-design-prototype/') :
-//     //     require __DIR__.'/views/website-design-prototype.php';
-//     //     break;
-    
-//     case addslashes('/website-design/') :
-//         require __DIR__.'/landing2/web-design-development/index.php';
-//         break;
-//     default:
-//         http_response_code(404);
-//         require __DIR__ . '/views/404.php';
-//         break;    
-// }
-
-?>
 
 <?php
 ob_start();
@@ -99,6 +5,11 @@ session_start();
 
 
 $url = $_SERVER['REQUEST_URI'];
+
+if(strpos($url,'packages')){
+    echo "pacakages";
+    die();
+}
 
 $current_url = explode('?', $url);
 $url = $current_url[0];
