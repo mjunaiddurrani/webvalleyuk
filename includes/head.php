cc7a934b7da88300470a43c106aad13dbcfa80cb
akashrajput9 <akashrajput9@hotmail.com>
2021-05-07 17:09:58 +0500

    udpate

--- includes/head.php
+++ includes/head.php
@@ -52,8 +52,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   <!-- seo  -->
   <meta property="og:locale" content="en_GB" />
   <meta property="og:type" content="website" />
-  <meta property="og:title" content="Innovative Website Designing Services In UK | Website Valley" />
-  <meta property="og:description" content="Avail branding services including custom stationery design, brand identity design, business card design to upgrade your brand, and attract user's attention." />
+  <meta property="og:title" content="Website Valley: Developing the Websites. Apps. Brands." />
+  <meta property="og:description" content="Website Valley is a professional web design, app development, and branding agency in the UK. Get our services on your budget, timeline, and specifications." />
   <meta property="og:url" content="<?= $requesMet.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ?>" />
   <meta property="og:site_name" content="websitevalley" />
   <meta property="article:publisher" content="https://www.facebook.com/websitevalley" />
--- views/blogui.php
+++ views/blogui.php
@@ -1,8 +1,6 @@
 <!doctype html>
 <html lang="en-GB">
-
-<!-- Mirrored from websitevalley.co.uk/portfolio by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 21 Jan 2021 17:41:06 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
 <title>Website Valley's Portfolio is Built with Love </title>
 
@@ -12,9 +10,8 @@
 
 
 <?php include_once('includes/head.php');?>
-
-
-
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/duotone.css" integrity="sha384-R3QzTxyukP03CMqKFe0ssp5wUvBPEyy9ZspCB+Y01fEjhMwcXixTyeot+S40+AjZ" crossorigin="anonymous"/>
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/fontawesome.css" integrity="sha384-eHoocPgXsiuZh+Yy6+7DsKAerLXyJmu2Hadh4QYyt+8v86geixVYwFqUvMU8X90l" crossorigin="anonymous"/>
 
 <link rel="stylesheet" href="assets/css/blog.css">
 
@@ -40,11 +37,11 @@
 
 <section class=" ">
     <div class="container">
-        <div class="col-lg-8 p-0 ">
+        <div class="col-lg-12 p-0 ">
             <h2 class="bannerHeading">The Logo Valley Blog</h2>
             <p class="paragraph mt-2">Tips and ideas to help you delight your customers</p>
         </div>
-        <div class="buttons">
+        <div class="buttons col-lg-12 ">
             <div class="row">
                 <div class="col-lg-4  py-3">
                     <button class="btnTheme active btn btn-primary btn-lg">Customer Service</button>
@@ -74,10 +71,210 @@
         <hr class="bg-black h-1">
     </div>
  </section>
+<section class="blogPosts">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+            <div class="">
+    <div class="vc_column-inner vc_custom_1551281491454">
+        <div class="wpb_wrapper">
+            <style media="screen">
+                .blog-posts-Z6NLEmxQiW .post-title :hover,
+                .blog-posts-Z6NLEmxQiW .post-title :hover * {
+                    color: #1093ea;
+                }
 
+                .blog-posts-Z6NLEmxQiW .post-thumbnail .post-categories a:hover,
+                .blog-posts-Z6NLEmxQiW .post-gallery .post-categories a:hover,
+                .blog-posts-Z6NLEmxQiW .blog-slider-item .post-categories a:hover,
+                .blog-posts-Z6NLEmxQiW .post-style-cover .post-categories a:hover,
+                .sh-post-categories-style2 .blog-posts-Z6NLEmxQiW .post-format-icon:hover {
+                    background-color: #1093ea;
+                }
+            </style>
+            <div class="sh-blog-standard-posts blog-posts-Z6NLEmxQiW">
+                <div class="sh-group blog-list blog-style-large blog-style-large-element blog-dividing-line-off">
+                    <article id="post-476" class="post-item post-476 post type-post status-publish format-standard has-post-thumbnail hentry category-beauty tag-shufflehound">
+                        <div class="post-container">
+                            <div class="post-categories-container">
+                                <div class="post-categories"><a href="https://gillion.shufflehound.com/journal/category/beauty/">Beauty</a></div>
+                            </div>
+                            <a href="https://gillion.shufflehound.com/journal/2020/01/20/website-that-you-should-check-out/" class="post-title">
+                                <h2>
+                                    Website that you should check out&nbsp;<span class="post-read-later post-read-later-guest tooltipstered" href="#login-register" data-type="add" data-id="476"><i class="fa fa-bookmark-o"></i></span>
+                                </h2>
+                            </a>
+                            <div class="post-meta">
+                                <div class="post-meta-content">
+                                    <span class="post-auhor-date">
+                                        <span> <a href="https://gillion.shufflehound.com/journal/author/gillionshufflehound/" class="post-author">Gillion</a></span>,
+                                        <a href="https://gillion.shufflehound.com/journal/2020/01/20/website-that-you-should-check-out/" class="post-date"> 1 year ago </a>
+                                    </span>
+                                    <a href="https://gillion.shufflehound.com/journal/2020/01/20/website-that-you-should-check-out/#comments" class="post-comments"> <i class="icon icon-bubble"></i> 1 </a>
+                                    <span class="post-readtime"> <i class="icon icon-clock"></i> 3 min <span>read</span> </span>
+                                    <span class="responsive-post-read-later">
+                                        &nbsp;<span class="post-read-later post-read-later-guest tooltipstered" href="#login-register" data-type="add" data-id="476"><i class="fa fa-bookmark-o"></i></span>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="post-thumbnail">
+                                <img
+                                    width="1200"
+                                    height="675"
+                                    src="https://cdn.gillion.shufflehound.com/wp-content/uploads/sites/16/2017/01/nicole-honeywill-1274515-unsplash-1200x675.jpg"
+                                    class="attachment-gillion-landscape-large size-gillion-landscape-large wp-post-image"
+                                    alt=""
+                                    loading="lazy"
+                                />
+                                <a href="https://gillion.shufflehound.com/journal/2020/01/20/website-that-you-should-check-out/" class="post-overlay">
+                                    <div class="post-overlay-content"><span></span> <span></span> <span></span></div>
+                                </a>
+                            </div>
+                            <div class="post-content-container">
+                                <div class="post-content"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet, nunc et accumsan cursus, neque eros sodales lectus, in fermentum…</p></div>
+                            </div>
+                            <div class="post-readmore sh-table">
+                                <div class="sh-table-cell post-readmore-text">
+                                    <a href="https://gillion.shufflehound.com/journal/2020/01/20/website-that-you-should-check-out/"><h6>Read more</h6> </a>
+                                </div>
+                                <div class="sh-table-cell post-readmore-line"><div class="post-readmore-line-content"></div></div>
+                                <div class="sh-table-cell">
+                                    <div
+                                        class="post-content-share post-content-share-side jssocials"
+                                        data-url="https://gillion.shufflehound.com/journal/2020/01/20/website-that-you-should-check-out/"
+                                        data-title="Website that you should check out"
+                                    >
+                                        <div class="jssocials-shares">
+                                            <div class="jssocials-share jssocials-share-twitter">
+                                                <a
+                                                    target="_blank"
+                                                    href="https://twitter.com/share?url=https%3A%2F%2Fgillion.shufflehound.com%2Fjournal%2F2020%2F01%2F20%2Fwebsite-that-you-should-check-out%2F&amp;text=Website%20that%20you%20should%20check%20out"
+                                                    class="jssocials-share-link"
+                                                >
+                                                    <i class="fa fa-twitter jssocials-share-logo"></i>
+                                                </a>
+                                            </div>
+                                            <div class="jssocials-share jssocials-share-facebook">
+                                                <a
+                                                    target="_blank"
+                                                    href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fgillion.shufflehound.com%2Fjournal%2F2020%2F01%2F20%2Fwebsite-that-you-should-check-out%2F"
+                                                    class="jssocials-share-link"
+                                                >
+                                                    <i class="fa fa-facebook jssocials-share-logo"></i>
+                                                </a>
+                                            </div>
+                                            <div class="jssocials-share jssocials-share-pinterest">
+                                                <a
+                                                    target="_blank"
+                                                    href="https://pinterest.com/pin/create/bookmarklet/?&amp;url=https%3A%2F%2Fgillion.shufflehound.com%2Fjournal%2F2020%2F01%2F20%2Fwebsite-that-you-should-check-out%2F&amp;description=Website%20that%20you%20should%20check%20out"
+                                                    class="jssocials-share-link"
+                                                >
+                                                    <i class="fa fa-pinterest jssocials-share-logo"></i>
+                                                </a>
+                                            </div>
+                                            <div class="jssocials-share jssocials-share-whatsapp">
+                                                <a
+                                                    target="_self"
+                                                    href="https://web.whatsapp.com/send?text=Gillion%20Journal%20Demo%20%E2%80%93%20Just%20another%20Gillion%20Theme%20site%0D%0Ahttps://gillion.shufflehound.com/journal/"
+                                                    class="jssocials-share-link"
+                                                >
+                                                    <i class="fa fa-whatsapp jssocials-share-logo"></i>
+                                                </a>
+                                            </div>
+                                            <div class="jssocials-share jssocials-share-messenger">
+                                                <a target="_self" href="fb-messenger://share?link=https%3A%2F%2Fgillion.shufflehound.com%2Fjournal%2F2020%2F01%2F20%2Fwebsite-that-you-should-check-out%2F" class="jssocials-share-link">
+                                                    <i class="fa fa-commenting jssocials-share-logo"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+        
+                </div>
+                <div class="sh-load-more" data-categories="" data-post-style="large" data-posts-per-page="6" data-paged="2" data-offset="0" data-id="blog-posts-Z6NLEmxQiW">Load more</div>
+            </div>
+        </div>
+    </div>
+</div>
 
+            </div>
+            <div class="col-lg-4">
+            <div class="theiaStickySidebar" style="padding-top: 0px; padding-bottom: 1px; position: static; transform: none; top: 0px; left: 1168.86px;">
+    <div class="vc_column-inner vc_custom_1551281499926">
+        <div class="wpb_wrapper">
+            <div class="wpb_widgetised_column wpb_content_element">
+                <div class="wpb_wrapper">
+                    <div id="social_counter-1" class="widget_advertise widget-item widget_social_counter">
+                        <div class="sh-widget-title-styling"><h3 class="widget-title">Stay connected</h3></div>
+                        <div class="sh-widget-connected-list sh-widget-connected-columns-3 sh-widget-connected-style2">
+                            <a href="https://facebook.com//" target="_blank" class="sh-widget-connected-item">
+                                <div class="sh-widget-connected-bubble sh-widget-connected-facebook">
+                                    <i class="fa fa-facebook"></i>
+                                    <div class="sh-widget-connected-bubble-count sh-widget-connected-facebook">1423</div>
+                                </div>
+                                <div class="sh-widget-connected-title sh-heading-font">Like</div>
+                            </a>
+                            <a href="https://twitter.com//" target="_blank" class="sh-widget-connected-item">
+                                <div class="sh-widget-connected-bubble sh-widget-connected-twitter">
+                                    <i class="fa fa-twitter"></i>
+                                    <div class="sh-widget-connected-bubble-count sh-widget-connected-twitter">727</div>
+                                </div>
+                                <div class="sh-widget-connected-title sh-heading-font">Follow</div>
+                            </a>
+                            <a href="https://www.instagram.com" target="_blank" class="sh-widget-connected-item">
+                                <div class="sh-widget-connected-bubble sh-widget-connected-instagram">
+                                    <i class="fa fa-instagram"></i>
+                                    <div class="sh-widget-connected-bubble-count sh-widget-connected-instagram">386</div>
+                                </div>
+                                <div class="sh-widget-connected-title sh-heading-font">Follow</div>
+                            </a>
+                        </div>
+                    </div>
+                    <div id="about_us-2" class="widget_about_us widget-item widget_about_us">
+                        <div class="sh-widget-title-styling"><h3 class="widget-title">Sarah Blackwing</h3></div>
+                        <a href=""> <img src="https://cdn.gillion.shufflehound.com/wp-content/uploads/sites/16/2019/02/journal_person.png" alt="Sarah Blackwing" /> </a>
+                        <p class="widget-description">Sometimes it is better to just walk away from things and go back to them later when you’re in a better frame of mind.</p>
+                        <a href="#" class="widget-learn-more"> Learn more </a>
+                    </div>
+                    <div id="subscribe-1" class="widget_sh_mailChimp widget_sh_mailChimp_style2 style3 widget-item widget_subscribe">
+                        <div class="sh-widget-title-styling"><h3 class="widget-title">Subscribe for newsletter</h3></div>
+                        <form id="mc4wp-form-2" class="mc4wp-form mc4wp-form-848" method="post" data-id="848" data-name="Journal Form">
+                            <div class="mc4wp-form-fields">
+                                <p><input type="email" name="EMAIL" placeholder="Your email address" required="" /></p>
+                                <p><button type="submit">Sign up</button></p>
+                            </div>
+                            <label style="display: none !important;">Leave this field empty if you're human: <input type="text" name="_mc4wp_honeypot" value="" tabindex="-1" autocomplete="off" /></label>
+                            <input type="hidden" name="_mc4wp_timestamp" value="1620365235" /><input type="hidden" name="_mc4wp_form_id" value="848" /><input type="hidden" name="_mc4wp_form_element_id" value="mc4wp-form-2" />
+                            <div class="mc4wp-response"></div>
+                        </form>
+                        <p class="widget-quote-description">* You will receive the latest news and updates on your favorite celebrities!</p>
+                    </div>
+               
+                    <div id="image-1" class="widget_sh_image widget-item widget_image">
+                        <a href=""> <img src="https://cdn.gillion.shufflehound.com/wp-content/uploads/sites/16/2018/05/ban_w2.png" class="" /> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="resize-sensor" style="position: absolute; inset: 0px; overflow: hidden; z-index: -1; visibility: hidden;">
+        <div class="resize-sensor-expand" style="position: absolute; left: 0; top: 0; right: 0; bottom: 0; overflow: hidden; z-index: -1; visibility: hidden;">
+            <div style="position: absolute; left: 0px; top: 0px; transition: all 0s ease 0s; width: 313px; height: 1969px;"></div>
+        </div>
+        <div class="resize-sensor-shrink" style="position: absolute; left: 0; top: 0; right: 0; bottom: 0; overflow: hidden; z-index: -1; visibility: hidden;">
+            <div style="position: absolute; left: 0; top: 0; transition: 0s; width: 200%; height: 200%;"></div>
+        </div>
+    </div>
+</div>
 
-
+            </div>
+        </div>
+    
+    </div>
+</section>
 
 
 <?php include_once('includes/footer.php');?>
