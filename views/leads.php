8c3faa331929ea61e5efad6a026833bf473481ba
Murtuza Mehdi <murtuzamehdi334@gmail.com>
2021-10-18 13:48:30 +0500

    make changes for dynamic payments

--- views/leads.php
+++ views/leads.php
@@ -30,7 +30,7 @@ $data=array(
 $payload=json_encode($data);
 $curl = curl_init();
 curl_setopt_array($curl, array(
-CURLOPT_URL => "https://dashboard.ghostwritingfounder.com/api/customer",
+CURLOPT_URL => "https://dashboard.ourbase.camp/api/customer",
 CURLOPT_RETURNTRANSFER => true,
 CURLOPT_ENCODING => "",
 CURLOPT_MAXREDIRS => 10,
