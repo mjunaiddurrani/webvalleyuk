47953c40294b9f947126c9117ff0a31c1ce708be
akashrajput9 <akashrajput9@hotmail.com>
2021-10-26 16:40:55 +0500

    session

--- views/leads.php
+++ views/leads.php
@@ -1,14 +1,9 @@
 <?php
-
-if ( ! session_id() ) {
-
-    session_start();
-    
-}
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+
 var_dump($_SESSION);die;
 
 
