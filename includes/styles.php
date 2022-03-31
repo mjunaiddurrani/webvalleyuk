a39f15cbd953ab55fddb1e214670e85d4f08b60a
mjunaiddurrani <mjunaddurrani@gmail.com>
2022-03-31 14:31:02 +0100

    live changes

--- includes/styles.php
+++ includes/styles.php
@@ -24,23 +24,10 @@ if (isset($_SERVER['HTTPS'])) {
 <link rel="stylesheet" href="css/nav.css">
 
 <!-- cards -->
-<meta property="og:locale" content="en_UK" />
-<meta property="og:type" content="website" />
-<meta property="og:title" content="Logo Designing Services By Professional Logo Design Experts For Hire" />
-<meta property="og:description" content="Custom Logo Designers at your service! Our Logo designing experts provide affordable and market-competitive logo design solutions. Learn for yourself. -description " />
-<meta property="og:url" content="https://www.getyourlogo.co.uk/" />
-<meta property="og:site_name" content="getYourLogo" />
-<meta property="article:publisher" content="https://www.facebook.com/Get-Your-Logo-530155357940008/?business_id=506098247426286" />
-<meta property="og:image" content="https://www.getyourlogo.co.uk/images/home/logo.png" />
-<meta name="twitter:card" content="summary_large_image" />
-<meta name="twitter:creator" content="@getyourlogo" />
-<meta name="twitter:site" content="@getyourlogo" />
-<meta name="twitter:image:src" content="https://www.getyourlogo.co.uk/images/home/logo.png">
-<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
-<link rel="manifest" href="images/site.webmanifest">
-<link rel="mask-icon" href="images/safari-pinned-tab.svg" color="#5bbad5">
+
+
+<link rel="icon" type="image/png" href="https://www.websitevalley.co.uk/favicon.png">
+
 <link rel="stylesheet" href="css/icofont.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" />
--- stripe/js/client.js
+++ stripe/js/client.js
@@ -1,74 +1,64 @@
 var domainName = 'https://www.websitevalley.co.uk/';
-$.get('https://dashboard.ourbase.camp/api/accountkey', { brandurl: domainName }, function(msg, status) {
-    if (status == "success") {
-        console.log(msg[0]['publishable_key']);
-        pk = msg[0]['publishable_key'];
-
-        var stripe = Stripe(pk);
-        // var stripe = Stripe("pk_live_51IHtDwCqRn7S2f1udaPu306FkNTfg75KhtQ6N3w1DHCOK1ROPncTfCfi7BtlnKpIvvP0354C3gvnbFHQQ1yt88sT000Ok9hoxO");
-        var elements = stripe.elements();
-        var style = { base: { fontSize: "16px", color: "#000" } };
-        var card = elements.create("card", { style: style });
-        card.mount("#card-element");
-        var form = document.getElementById("payment-form");
-        form.addEventListener("submit", function(event) {
-            event.preventDefault();
-            stripe.createToken(card).then(function(result) {
-                if (result.error) {
-                    $.get("error_log.php", { code: result.error.code, message: result.error.message }, function(data, status) {
-                        if (status == "success") {
-                            var errorElement = document.getElementById("card-errors");
-                            errorElement.textContent = result.error.message;
-                            // swal({
-                            //     position: 'top-end',
-                            //     icon: 'warning',
-                            //     title: result.error.message
-                            // });
-                            Swal.fire({
-                                position: 'top-end',
-                                text: result.error.message,
-                                type: "warning",
-                            });
-                            $('.submitPay').prop('disabled', false);
-                        }
+var stripe = Stripe("pk_live_51KOV7ZIWAWD6Rl50hMBF5jzDaCRDLVTkpNEjJpbHQ7YCLEUu92A8m5iKarHTaoaW8LrtRfVF7EWwWxVN6hnlYIZ200UzyOc1ay");
+var elements = stripe.elements();
+var style = { base: { fontSize: "16px", color: "#000" } };
+var card = elements.create("card", { style: style });
+card.mount("#card-element");
+var form = document.getElementById("payment-form");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    stripe.createToken(card).then(function(result) {
+        if (result.error) {
+            $.get("error_log.php", { code: result.error.code, message: result.error.message }, function(data, status) {
+                if (status == "success") {
+                    var errorElement = document.getElementById("card-errors");
+                    errorElement.textContent = result.error.message;
+                    // swal({
+                    //     position: 'top-end',
+                    //     icon: 'warning',
+                    //     title: result.error.message
+                    // });
+                    Swal.fire({
+                        position: 'top-end',
+                        text: result.error.message,
+                        type: "warning",
                     });
-
-                } else {
-                    stripeTokenHandler(result.token);
+                    $('.submitPay').prop('disabled', false);
                 }
             });
-        });
 
-        function stripeTokenHandler(token) {
-            var form = document.getElementById("payment-form");
-
-            var hiddenToken = document.createElement("input");
-            hiddenToken.setAttribute("type", "hidden");
-            hiddenToken.setAttribute("name", "stripeToken");
-            hiddenToken.setAttribute("value", token.id);
-            form.appendChild(hiddenToken);
-
-            var hiddenLast = document.createElement("input");
-            hiddenLast.setAttribute("type", "hidden");
-            hiddenLast.setAttribute("name", "last4");
-            hiddenLast.setAttribute("value", token.card.last4);
-            form.appendChild(hiddenLast);
-
-            var hiddenMonth = document.createElement("input");
-            hiddenMonth.setAttribute("type", "hidden");
-            hiddenMonth.setAttribute("name", "exp_month");
-            hiddenMonth.setAttribute("value", token.card.exp_month);
-            form.appendChild(hiddenMonth);
-
-            var hiddenYear = document.createElement("input");
-            hiddenYear.setAttribute("type", "hidden");
-            hiddenYear.setAttribute("name", "exp_year");
-            hiddenYear.setAttribute("value", token.card.exp_year);
-            form.appendChild(hiddenYear);
-
-            form.submit();
+        } else {
+            stripeTokenHandler(result.token);
         }
-    } else {
-        console.log('api is not working');
-    }
+    });
 });
+
+function stripeTokenHandler(token) {
+    var form = document.getElementById("payment-form");
+
+    var hiddenToken = document.createElement("input");
+    hiddenToken.setAttribute("type", "hidden");
+    hiddenToken.setAttribute("name", "stripeToken");
+    hiddenToken.setAttribute("value", token.id);
+    form.appendChild(hiddenToken);
+
+    var hiddenLast = document.createElement("input");
+    hiddenLast.setAttribute("type", "hidden");
+    hiddenLast.setAttribute("name", "last4");
+    hiddenLast.setAttribute("value", token.card.last4);
+    form.appendChild(hiddenLast);
+
+    var hiddenMonth = document.createElement("input");
+    hiddenMonth.setAttribute("type", "hidden");
+    hiddenMonth.setAttribute("name", "exp_month");
+    hiddenMonth.setAttribute("value", token.card.exp_month);
+    form.appendChild(hiddenMonth);
+
+    var hiddenYear = document.createElement("input");
+    hiddenYear.setAttribute("type", "hidden");
+    hiddenYear.setAttribute("name", "exp_year");
+    hiddenYear.setAttribute("value", token.card.exp_year);
+    form.appendChild(hiddenYear);
+
+    form.submit();
+}
