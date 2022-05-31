cd109c4e92aa08ba61d42d70c088688febc65517
Murtuza Mehdi <murtuzamehdi334@gmail.com>
2022-05-31 16:09:52 +0500

    udpsate

--- views/leads.php
+++ views/leads.php
@@ -2,8 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-ob_start();
-session_start();
+// ob_start();
+// session_start();
 
 include_once($_SERVER['DOCUMENT_ROOT']."/includes/get-dashboard.php");
 
