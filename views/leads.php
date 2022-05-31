44e3f4ae1f9d97fccf5d31e770b0e879e96af7e9
Murtuza Mehdi <murtuzamehdi334@gmail.com>
2022-05-31 16:06:36 +0500

    updsate

--- views/leads.php
+++ views/leads.php
@@ -1,11 +1,11 @@
 <?php
-
-include_once($_SERVER['DOCUMENT_ROOT']."/includes/get-dashboard.php");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ob_start();
 session_start();
+
+include_once($_SERVER['DOCUMENT_ROOT']."/includes/get-dashboard.php");
 
 $name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
