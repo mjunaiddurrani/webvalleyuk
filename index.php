969ec6cc1a3bc50919373290dd3402ad714e28b7
akashrajput9 <akashrajput9@hotmail.com>
2022-06-08 17:16:49 +0500

    udaspt[

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
-
-
+if( empty(session_id()) && !headers_sent()){
+    session_start();
+}
 $url = $_SERVER['REQUEST_URI'];
 
 // if(strpos($url,'/packages')){
