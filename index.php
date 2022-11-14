bbc481d166d1c9af2b4158cc3f090cff699d6bee
mjunaiddurrani <mjunaiddurrani@hotmail.com>
2022-11-14 21:35:47 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 ini_set('display_errors', 0);
-ini_set('display_startup_errors', 1);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 
 $url = $_SERVER['REQUEST_URI'];
