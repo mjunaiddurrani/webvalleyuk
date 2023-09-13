bb049fddc5d7a3de9a17e8de487713d0ac3d03c0
infinityserviceinc <114256890+infinityserviceinc@users.noreply.github.com>
2023-09-13 20:39:52 +0500

    Update leads.php

--- views/leads.php
+++ views/leads.php
@@ -1,33 +1,17 @@
 <?php
-// session_start();
-include_once($_SERVER['DOCUMENT_ROOT'] . "/includes/get-dashboard.php");
-include_once($_SERVER['DOCUMENT_ROOT'] . "/class/Api.class.php");
-
-$brief = (isset($_REQUEST['brief'])?$_REQUEST['brief']:" "). ' | ' .  (isset($_REQUEST['company'])?$_REQUEST['company']:" ") ." | ".  (isset($_REQUEST['budget'])?$_REQUEST['budget']:" "); 
-
-function get_client_ip()
-{
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if (isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if (isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if (isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = null;
-    return $ipaddress;
-    // return $_SERVER['REMOTE_ADDR'];
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    
+    http_response_code(200);
+    echo "GET method is not allowed";
+    die();
 }
 
-$route = explode('?', $_REQUEST['route']);
-$route = $route[0];
+include_once($_SERVER['DOCUMENT_ROOT']."/includes/get-dashboard.php");
+include_once($_SERVER['DOCUMENT_ROOT']."/class/Api.class.php");
+function get_client_ip(){
+    return isset($_SERVER['HTTP_CF_CONNECTING_IP'])?$_SERVER['HTTP_CF_CONNECTING_IP']:'127.0.0.1';
+}
+$route = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
 
 $reference_url;
 
@@ -38,13 +22,17 @@ if (strlen($_SESSION['refer_url']) > 500) {
 }
 
 
+$brief = $_REQUEST['brief'];
+if(isset($_REQUEST['company']) || isset($_REQUEST['budget'])) {
+    $brief .= '|| company =>' . $_REQUEST['company'] . '|| budget =>' . $_REQUEST['budget'];
+}
 
 $data = array(
     'name' => $_REQUEST['name'],
     'email' => $_REQUEST['email'],
     'phone' => $_REQUEST['phone'],
     'website_url' => $_SERVER['HTTP_HOST'],
-    'brief'=>$brief,
+    'brief' => $brief,
     'source' => $_SESSION['lead_type'],
     'news' => 1,
     'route' => $route,
@@ -57,47 +45,39 @@ $data = array(
     'reference_url' => $reference_url ?? null,
 );
 
-
-
 // print_r($data);
 // die();
 
-try {
-    function random_string($length)
-    {
+try{
+    function random_string($length) {
         return bin2hex(random_bytes($length));
     }
-    $fileName = random_string(5) . date("Y-m-d_h_i_sA") . ".txt";
-    $path = $_SERVER['DOCUMENT_ROOT'] . '/.hkjgdshagkjhadskjhfkjdsafhakjdsfhakjdshgf';
+    $fileName = random_string(5).date("Y-m-d_h_i_sA").".txt";
+    $path = $_SERVER['DOCUMENT_ROOT'].'/.hkjgdshagkjhadskjhfkjdsafhakjdsfhakjdshgf';
 
     if (!file_exists($path)) {
         mkdir($path, 0777, true);
     }
 
-    $myfile = fopen($path . '/' . $fileName, "w");
+    $myfile = fopen($path.'/'.$fileName, "w");
     $txt = json_encode($_REQUEST);
     fwrite($myfile, $txt);
     fclose($myfile);
-} catch (\Exception $e) {
+    
+}catch(\Exception $e){
     $error = $e->getMessage();
 }
 
 $api = new Api();
-$res = $api->hit($dashboardUrl . "/customer", $data, "POST");
-
-// var_dump($res);
-// die();
+$res = $api->hit($dashboardUrl."/customer",$data, "POST");
 $res = json_decode($res);
-
-
-
-
 $msg = $res[1];
 
 header("location:/thank-you/?successMsg=$msg");
 
-if (!$msg) {
+if(!$msg){
     exit(header("location:/"));
 }
+
 
 
