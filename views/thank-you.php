d90b45e573b78eab7c20b745eebd5e0fd476756c
abrarraza1 <abrar123raza@gmail.com>
2023-12-01 00:55:44 +0500

    responsive

--- views/thank-you.php
+++ views/thank-you.php
@@ -6,10 +6,16 @@
   <!-- <link rel="stylesheet" href="css/icofont.min.css"> -->
     <title>website valley Thank you page</title>
 
+
+    <style>
+      .thankyou_sec i.fab {
+          font-family: FontAwesome !important;
+      }
+    </style>
 </head>
   <body>
     <?php include_once('includes/header.php') ?>
-    <section class="section1" style="background: #2A4B67;min-height:425px;">
+    <section class="section1 thankyou_sec" style="background: #2A4B67;min-height:425px;">
       <div class="container">
         <div class="row text-center text-sm-left my-auto">
           <div class="col-lg-9">
